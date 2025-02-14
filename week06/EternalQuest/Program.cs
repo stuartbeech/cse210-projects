bfd42af0cb1cb6b4638b9b7252a0f8f438f17244
stuartbeech <sbeech@byupathway.edu>
2025-02-14 10:26:36 +0200

    Added comments for exceeding requirements

--- week06/EternalQuest/Program.cs
+++ week06/EternalQuest/Program.cs
@@ -2,6 +2,10 @@ using System;
 
 class Program
 {
+    // Exceeding requirements:
+    // Added logic to display star animations when a user completes the checklist goals
+    // and receives the additional bonus
+
     static void Main(string[] args)
     {
         // Console.WriteLine("Hello World! This is the EternalQuest Project.");
